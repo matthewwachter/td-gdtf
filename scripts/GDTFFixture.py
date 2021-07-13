a9b51b52d93e8c7b521a202183d303a6c75ace6d
matthewwachter <matthewwachter@gmail.com>
2021-07-13 14:08:20 -0700

    get component to import basic channels of fixture

--- scripts/GDTFFixture.py
+++ scripts/GDTFFixture.py
@@ -1,10 +1,16 @@
 import traceback
 
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, project.folder+'/modules')
+
 import pygdtf
 
 from CallbacksExt import CallbacksExt
 from TDStoreTools import StorageManager
 TDF = op.TDModules.mod.TDFunctions
+
+from pprint import pprint
 
 class GDTFFixture(CallbacksExt):
 	"""
@@ -41,7 +47,7 @@ class GDTFFixture(CallbacksExt):
 		# stored items (persistent across saves and re-initialization):
 		storedItems = [
 			# Only 'name' is required...
-			{'name': 'Data', 'default': None, 'readOnly': False,
+			{'name': 'Channels', 'default': [], 'readOnly': False,
 			 						'property': True, 'dependable': True},
 		]
 		self.fixture = None
@@ -59,12 +65,109 @@ class GDTFFixture(CallbacksExt):
 			if r.group == group_name:
 				r.kill()
 
+	def destroyChans(self):
+		self.stored['Channels'] = []
+		pages = [p.name for p in self.ownerComp.customPages]
+		dmx_page = self.ownerComp.customPages[pages.index('DMX')]
+		for p in dmx_page.pars:
+			#print(p.name)
+			p.destroy()
+
+	def updateMode(self, mode):
+		#print(type(dmx_page))
+
+		self.destroyChans()
+
+		pages = [p.name for p in self.ownerComp.customPages]
+		dmx_page = self.ownerComp.customPages[pages.index('DMX')]
+
+		channels = []
+		m = self.fixture.dmx_modes[mode]
+		for c in m.dmx_channels:
+			
+			logical_chan = c.logical_channels[0]
+			offset = c.offset
+			name = logical_chan.attribute.str_link
+			p_name = name.lower().capitalize().replace('_', '')
+			
+			#pprint(logical_chan.channel_functions[0].__dict__)
+
+			channel_function = logical_chan.channel_functions[0]
+
+			byte_count = channel_function.dmx_from.byte_count
+
+			print(byte_count)
+			default = channel_function.default.value
+
+			if len(logical_chan.channel_functions) < 2:
+				physical_from = channel_function.physical_from
+				physical_to = channel_function.physical_to
+			else:
+				physical_from = 0
+				physical_to = 255**byte_count
+
+			default = tdu.remap(default, 0, 256**byte_count,physical_from, physical_to)	
+			
+
+			#pprint(channel_function.mode_to.__dict__)
+			print(name, default)
+
+			p = dmx_page.appendFloat(
+				p_name,
+				label = name
+			)[0]
+
+			p.min = physical_from
+			p.normMin = physical_from
+			p.default = default
+			p.val = default
+			p.max = physical_to
+			p.normMax = physical_to
+			p.order = offset[0]
+
+			channels.append(
+				{
+					'offset': offset,
+					'name': name,
+					'p_name': p_name,
+					'byte_count': byte_count,
+					'default': default,
+					'physical_from': physical_from,
+					'physical_to': physical_to
+				}
+			)
+
+
+
+			
+
+		self.stored['Channels'] = channels
+		# for m in self.fixture.dmx_modes[0]:
+			
+		# 	print(m.name)
+
+		# 	self.ownerComp.par.Mode
+
+		#print(dmx_page)
+		pass
+
 	def Load(self, file=None):
 		if file is None:
 			file = self.ownerComp.par.Gdtffile.eval()
 		self.fixture = pygdtf.FixtureType(file)
 
-		print(self.fixture.dmx_profiles[0])
+		# print(len(self.fixture.dmx_modes))
+
+		self.ownerComp.par.Mode.menuNames = [str(i) for i in range(len(self.fixture.dmx_modes))]
+		self.ownerComp.par.Mode.menuLabels = [m.name for m in self.fixture.dmx_modes]
+		mode = 0
+		self.ownerComp.par.Mode = mode
+		# self.ownerComp.par.Mode.cook()
+
+		#print(dir(self.fixture.dmx_modes[0]))
+
+		self.updateMode( mode )
+		
 
 	# example method with callback
 	def Start(self):
@@ -84,3 +187,6 @@ class GDTFFixture(CallbacksExt):
 
 	def pulse_Load(self):
 		self.Load()
+
+	def pulse_Destroychans(self):
+		self.destroyChans()
